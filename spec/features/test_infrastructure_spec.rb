15c1891e145916cba5b551e8f3700e4d259ef329
Alex L <macbookpro@macbooks-mbp.home>
2020-04-03 12:05:58 +0100

    test - page has content what's your name - fail

--- spec/features/test_infrastructure_spec.rb
+++ spec/features/test_infrastructure_spec.rb
@@ -4,3 +4,10 @@ feature "Page says Hello there!" do
     expect(page).to have_content 'Hello there!'
   end
 end
+
+feature "Page asks for the persons name" do
+  scenario "page has content 'What's your name'" do
+    visit('/')
+    expect(page).to have_content "What's your name"
+  end
+end
